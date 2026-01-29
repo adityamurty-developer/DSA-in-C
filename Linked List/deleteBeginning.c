8de37ce88b7bffd423e75983489b1dd37f0734a9
R Aditya Murty <adityamurty2006@gmail.com>
2026-01-29 23:11:44 +0530

    Delete a node

---
+++ Linked List/deleteBeginning.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *head=NULL,*temp,*newnode;
+    int n,i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("Enter data: ");
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+
+        if(head==NULL){
+            head=newnode;
+        }else{
+            temp=head;
+            while(temp->next!=NULL){
+                temp=temp->next;
+            }
+            temp->next=newnode;
+        }
+    }
+
+    temp = head;
+    head = head->next; 
+    free(temp);        
+
+    printf("After Deletion: ");
+    temp = head;
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    return 0;
+}
